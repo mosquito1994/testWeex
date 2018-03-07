fa2e130e9d9add19d46d7384086ac59f30c5c1e7
hzchenshiwen <hzchenshiwen@corp.netease.com>
2018-03-07 10:27:08 +0800

    debug:

--- configs/scripts.js
+++ configs/scripts.js
@@ -1,2 +1,9 @@
 const webpack = require('webpack');
 const argv = require('yargs').argv
+
+function rGetFolder(_dir) {
+    return fs.readdirSync(_dir)
+        .filter((_file) => {
+            return fs.statSync(path.join(_dir, _file)).isDirectory()
+        })
+}
